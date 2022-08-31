b3d5373319257db8061e8504dd9207efe81252e4
Maxim Hladchuk <maxim.hladchuk@gmail.com>
2022-08-31 17:25:22 +0300

    modificated modal window (without Js)

---
+++ app/code/CustomMax/main-page-popup/view/frontend/web/js/myscript.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mainModalWindowCheckStatus = false;
+// require([
+//         "jquery",
+//         "Magento_Ui/js/modal/modal"
+//     ],
+//
+//     function ($, modal
+//     ) {
+//
+//         var options = {
+//             type: 'popup',
+//             responsive: true,
+//             autoOpen: true,
+//             title: '',
+//             clickableOverlay: false,
+//             buttons: [{
+//                 text: $.mage.__('Close'),
+//                 class: '',
+//                 click: function () {
+//                     if (mainModalWindowCheckStatus) {
+//                         this.closeModal();
+//                     }
+//                 }
+//             }, {
+//                 text: $.mage.__('I`m older than 18'),
+//                 class: '',
+//                 click: function () {
+//                     mainModalWindowCheckStatus = true;
+//                 }
+//             }]
+//         };
+//
+//         var popup = modal(options, $('#modal'));
+//         $("#button").click(function() {
+//             $('#modal').modal('openModal');
+//         });
+//     })
+// ;
